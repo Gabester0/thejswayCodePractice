c11837c894ab6ed134819b8534be26f349065dbd
Gabe Eipper <30967371+Gabester0@users.noreply.github.com>
2018-04-14 23:09:00 -0400

    Add files via upload
    3 2 1 Lets Code!

---
+++ chapter1script.js
@@ -0,0 +1,27 @@
+//Simple program to display name and age
+
+console.log("My name is Gabe\n and I am 30");
+////Actual result: success! \n worked and my name and age appeared on separate lines
+
+//minimalistic calculator
+
+console.log(6 +3);
+console.log(6-3);
+console.log(6*3);
+console.log(6/3);
+////Actual result: success
+
+
+//predictive
+
+console.log(4 + 5);
+//result guess: 9
+////Actualresult: 9
+
+console.log("4 + 5");
+//result guess: 4 + 5
+////Actual result: 4 +5
+
+console.log("4" + "5");
+//result guess: 45
+////Actual result: 45
